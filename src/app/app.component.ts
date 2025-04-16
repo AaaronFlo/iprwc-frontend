9b65abb9d756983c80dc6eb174f24785bb519e0c
Aaron Flores <s1127388@student.hsleiden.nl>
2025-04-16 23:21:40 +0200

    Afgerond frontend!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NewProductComponent } from './components/products/new-product/new-product.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { LoginpageComponent } from './components/login/loginpage/loginpage.component';
+import { RegisterpageComponent } from './components/login/registerpage/registerpage.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  templateUrl:'./app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+import { NewProductComponent } from './components/products/new-product/new-product.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { AdminportalComponent } from './components/portals/adminportal/adminportal.component';
+import { UserportalComponent } from './components/portals/userportal/userportal.component';
+import { AuthGuard } from './components/login/auth.guard';
+import { LoginpageComponent } from './components/login/loginpage/loginpage.component';
+import { RegisterpageComponent } from './components/login/registerpage/registerpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: 'login',
+    component: LoginpageComponent,
+},
+{
+    path: 'register',
+    component: RegisterpageComponent,
+},
+{
+    path: 'products',
+    component: ProductsComponent,
+    canActivate: [AuthGuard]
+},
+{
+    path: 'addProduct',
+    component: NewProductComponent,
+    canActivate: [AuthGuard]
+},
+{
+    path: 'shopping-cart',
+    component: ShoppingCartComponent,
+    canActivate: [AuthGuard]
+},
+{
+    path: 'admin-portal',
+    component: AdminportalComponent,
+    canActivate: [AuthGuard]
+},
+{
+    path: 'user-portal',
+    component: UserportalComponent,
+    canActivate: [AuthGuard]
+},
+{path: '', redirectTo: 'login', pathMatch: 'full'},
+];
